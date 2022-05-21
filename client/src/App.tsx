efc5f480779274bdfba4cf97ec0bab297cd66b00
Yuvraj Singh <ysgaur9919@gmail.com>
2022-05-21 16:47:52 +0530

    devops: containerized client and server with docker and docker-compose

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="bg-gray-700 text-white p-4 rounded">
-      Hello from Curie React Client
+      Hello!, from Curie React Client
     </div>
   );
 }
