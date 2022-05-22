59bb1caab7bab5fa1f01f679ca16a1962502400e
Yuvraj Singh <ysgaur9919@gmail.com>
2022-05-22 19:15:39 +0530

    style: updated logo and built Main UI container

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
-    <div className="w-16 h-full rounded-bl-lg backdrop-blur-md bg-indigo-700/50 border border-l-0 border-y-0 border-indigo-300/30"></div>
+    <div className="w-16 h-full rounded-bl-lg backdrop-blur-md bg-indigo-700/50 border border-l-0 border-y-0 border-indigo-300/30">
+      {/* <Link to="/"></Link> */}
+    </div>
   );
 }
