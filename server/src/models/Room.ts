e2d9408cee9bd73f4faade4ca7b1336b367ebfea
Yuvraj Singh <ysgaur9919@gmail.com>
2022-06-24 12:23:22 +0530

    fix: fixed room schema

--- server/src/models/Room.ts
+++ server/src/models/Room.ts
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
+const Message = new mongoose.Schema({
+    type: { type: String, default: 'text' },
+    content: { type: String, required: true },
+    sender: { type: String, required: true }
+})
+
 const RoomSchema = new mongoose.Schema({
     _id: { type: String, required: true, unique: true },
     name: { type: String, required: true },
-    state: { type: String, default: "idle" },
     image_url: { type: String },
     users: [{ type: String }],
-    messages: [{ type: String }]
+    messages: [Message]
 })
 
 const Room = mongoose.model('room', RoomSchema);
