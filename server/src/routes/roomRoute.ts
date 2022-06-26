b23757970183182a5230174ea414a772d774163c
Yuvraj Singh <ysgaur9919@gmail.com>
2022-06-26 14:10:20 +0530

    create: created /remove_room and /remove_user routes

--- server/src/routes/roomRoute.ts
+++ server/src/routes/roomRoute.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createRoomController, deleteRoomController, getRoomController, joinUserController, updateRoomController } from '../controllers/roomController';
+import { createRoomController, deleteRoomController, getRoomController, joinUserController, removeUserController, updateRoomController } from '../controllers/roomController';
 const router = express.Router();
 
 router.get('/:rid', getRoomController);
 
 router.get('/join_user/:rid', joinUserController)
 
+router.get('/remove_user/:rid', removeUserController)
 
 router.get('/delete/:rid', deleteRoomController);
 
