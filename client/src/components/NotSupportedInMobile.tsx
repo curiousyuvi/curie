55fb21302f2aba0692d2b2371f664a7b7af54449
Yuvraj Singh <ysgaur9919@gmail.com>
2022-07-20 22:19:54 +0530

    fix: fixed some bugs

--- client/src/components/NotSupportedInMobile.tsx
+++ client/src/components/NotSupportedInMobile.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotSupportedInMobile = () => {
   return (
-    <div className="w-full h-full bg-indigo-800 justify-center items-center text-gray-100">
+    <div className="w-full h-full bg-indigo-800 justify-center items-center text-gray-100 p-4 text-center">
       <h1>
         {
           "😵 Currently, this app is not supported in Mobile Devices, please switch to Desktop or Laptop"
