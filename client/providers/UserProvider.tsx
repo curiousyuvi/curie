f447799fcf1b7a00fcd0ed5a44702c7b00242e70
Yuvraj Singh <ysgaur9919@gmail.com>
2022-09-06 11:17:14 +0530

    created UserProvider

---
+++ client/providers/UserProvider.tsx
@@ -0,0 +1,19 @@
+import React, { createContext, FC, ReactNode, useState } from "react";
+import { User } from "../interfaces/User";
+
+const userContext = createContext<User>({ name: "", imageUrl: "" });
+
+export type UserProviderProps = { children: ReactNode };
+
+const UserProvider: FC<UserProviderProps> = ({ children }) => {
+  const [name, setName] = useState<string>("");
+  const [imageUrl, setImageUrl] = useState<string>("");
+
+  return (
+    <userContext.Provider value={{ name, imageUrl }}>
+      {children}
+    </userContext.Provider>
+  );
+};
+
+export default UserProvider;
