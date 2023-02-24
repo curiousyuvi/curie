16c740543a02ebc7fc8b9e0990cadddc8257c3c5
Raj Gautam <raj350199@gmail.com>
2023-02-24 23:17:09 +0530

    some fixes

--- client/components/YoutubeEmbedPlayer.tsx
+++ client/components/YoutubeEmbedPlayer.tsx
@@ -4,14 +4,8 @@ import YouTube from "react-youtube";
 
 const YoutubeEmbedPlayer = () => {
   const debugEmbed = false;
-  const {
-    setPlayer,
-    setPaused,
-    currentTrack,
-    setCurrentTrack,
-    setProgress,
-    setDuration,
-  } = useRoomMusic();
+  const { setPlayer, setPaused, currentTrack, setProgress, setDuration } =
+    useRoomMusic();
 
   const handleStateChange = (event: any) => {
     console.log("state change", event);
@@ -27,7 +21,7 @@ const YoutubeEmbedPlayer = () => {
 
   const _onReady = (event: any) => {
     setPlayer(event.target);
-    event.target.pauseVideo();
+    // event.target.pauseVideo();
     setDuration(event.target.getDuration());
     setProgress(event.target.getCurrentTime());
   };
@@ -38,7 +32,7 @@ const YoutubeEmbedPlayer = () => {
       enablejsapi: 1 as 0 | 1,
       autoplay: 1 as 0 | 1,
       playsinline: 1 as 0 | 1,
-      loop: 1 as 0 | 1,
+      // loop: 1 as 0 | 1,
     },
   };
   return (
